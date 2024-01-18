ea94e1ba73b1ed6435201f09b979266883542f04
NeverEverFelix <134745489+NeverEverFelix@users.noreply.github.com>
2024-01-18 10:51:48 -0500

    Add files via upload

---
+++ random.c
@@ -0,0 +1,15 @@
+//
+//  random.c
+//  
+//
+//  Created by Felix M on 1/18/24.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <random.h>
+
+char randomchar(){
+    return char randomletter = 'A' + (random() % 26);
+}
